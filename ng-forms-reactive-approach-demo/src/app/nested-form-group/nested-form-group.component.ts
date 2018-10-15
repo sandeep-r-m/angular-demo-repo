07d2dd02d040e3e2da695b7f6ce628d389b61e8e
sandeep-r-m <srm4knowledge.com>
2018-10-15 22:21:17 +0530

    ng reactive approach to form handling - using FormArray to handle multiple user defined inputs

--- ng-forms-reactive-approach-demo/src/app/nested-form-group/nested-form-group.component.ts
+++ ng-forms-reactive-approach-demo/src/app/nested-form-group/nested-form-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form-group',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NestedFormGroupComponent implements OnInit {
 
+  genders = [
+    {
+      value: "M",
+      desc: "Male"
+    },
+    {
+      value: "F",
+      desc: "Female"
+    }
+  ];
+
+  signupForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+
+    //this.signupForm = new FormGroup({}); // empty {} tells there are no controls in the form
+
+    //Define controls in the form. Need to link these to html form.
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl('superuser-nested', Validators.required),
+        'gender': new FormControl('F', [Validators.required])
+      }),
+      'email': new FormControl('superuser-nested@foobar.com', [Validators.required, Validators.email]),
+      'hobbies': new FormArray([])
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.signupForm)
+    //this.signupForm.reset();
+  }
+
+  onAddHobby() {
+    const hobby = new FormControl('', Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(hobby);
   }
 
 }
